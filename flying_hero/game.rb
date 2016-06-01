604b61ec07fa564dc25d1d60037e840df9d08b5e
nicolaspavon <nicolaspavon33@gmail.com>
2016-06-01 19:49:46 -0300

    ggwp

---
+++ flying_hero/game.rb
@@ -0,0 +1,16 @@
+﻿require 'gosu'
+class Game < Gosu::Window
+	def initialize
+		super(900, 500, fullscreen: false)
+		self.caption = 'Flying hero!'
+	end
+	def draw
+
+	end
+	def button_down(id)
+		if id== Gosu::KbEscape
+			close
+		end
+	end
+
+end
